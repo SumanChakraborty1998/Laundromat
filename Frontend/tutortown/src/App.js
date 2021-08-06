d4e84d5978b3bbee6ed72026d5eedbb4ea0ff6f1
Shanoor24 <shanoorfb2403@gmail.com>
2021-08-07 02:41:52 +0530

    updated

--- Frontend/tutortown/src/App.js
+++ Frontend/tutortown/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import SignUpMain from './Components/SignUpPage/SignUpMain';
-// import HomeMainPage from './Components/HomePage/HomeMainPage';
+import HomeMainPage from './Components/HomePage/HomeMainPage';
 
 function App() {
   return (
     <div className="App">
-      {/* < HomeMainPage /> */}
-      <SignUpMain />
+      < HomeMainPage />
     </div>
   );
 }
