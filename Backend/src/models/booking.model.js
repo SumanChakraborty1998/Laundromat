9ecdd77382debe478acbef50f81dc2daf991a735
SumanChakraborty1998 <sumanchakra8145@gmail.com>
2021-08-06 23:31:51 +0530

    Booking and prices collection is now added

---
+++ Backend/src/models/booking.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema(
+    {
+        tutor: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "tutor",
+            required: true,
+        },
+        booked_by_student: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "student",
+            required: true,
+        },
+        place: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "place",
+            required: true,
+        },
+        subject: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "subject",
+            required: true,
+        },
+    },
+    { timestamps: true, versionKey: false },
+);
+
+const Booking = mongoose.model("booking", bookingSchema);
+module.exports = Booking;
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,10 +7,16 @@ app.use(express.json());
 const placesController = require("./controllers/place.controller");
 const subjectsController = require("./controllers/subject.controller");
 const tutorsController = require("./controllers/tutor.controller");
+const pricesController = require("./controllers/price.controller");
+const studentsController = require("./controllers/student.controller");
+const bookingsController = require("./controllers/booking.controller");
 
 app.use("/places", placesController);
 app.use("/subjects", subjectsController);
 app.use("/tutors", tutorsController);
+app.use("/prices", pricesController);
+app.use("/students", studentsController);
+app.use("/bookings", bookingsController);
 
 const start = async () => {
     await connect();
