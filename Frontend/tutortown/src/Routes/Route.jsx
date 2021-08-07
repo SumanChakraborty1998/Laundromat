28b4272f1c1a409f3418a2b4e5e92f074f31edcc
SumanChakraborty1998 <sumanchakra8145@gmail.com>
2021-08-07 19:38:05 +0530

    Done

--- Frontend/tutortown/src/Routes/Route.jsx
+++ Frontend/tutortown/src/Routes/Route.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Admin } from "../Components/Admin/Admin";
+import HomeMainPage from "./../Components/HomePage/HomeMainPage";
+import SignUpMain from "./../Components/SignUpPage/SignUpMain";
+import ButtonAppBar from "./../Components/HomePage/ButtonAppBar";
+import { Footer } from "../Components/HomePage/Footer";
 
 const Routes = () => {
-  return (
-    <div>
-      <Switch>
-        <Route path="/admin/tutorTown/authenticated" exact>
-          <Admin />
-        </Route>
-      </Switch>
-    </div>
-  );
+    return (
+        <>
+            <ButtonAppBar />
+            <div>
+                <Switch>
+                    <Route path="/" exact>
+                        <HomeMainPage />
+                    </Route>
+                    <Route path="/signup" exact>
+                        <SignUpMain />
+                    </Route>
+                    <Route path="/admin/tutorTown/authenticated" exact>
+                        <Admin />
+                    </Route>
+                </Switch>
+            </div>
+            <Footer />
+        </>
+    );
 };
 
 export default Routes;
