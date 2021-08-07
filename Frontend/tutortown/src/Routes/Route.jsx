429a1ae2df5c8736e2c00e420eaba96d03df6926
SumanChakraborty1998 <sumanchakra8145@gmail.com>
2021-08-07 18:05:47 +0530

    Doing WebRtc part

--- Frontend/tutortown/src/Routes/Route.jsx
+++ Frontend/tutortown/src/Routes/Route.jsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Admin } from "../Components/Admin/Admin";
-=======
-import React from 'react';
-// import { Route, Switch } from 'react-router-dom';
-
->>>>>>> 4db99966359c7beb154e159011e04ef9842e7d11
 
 const Routes = () => {
     return (
