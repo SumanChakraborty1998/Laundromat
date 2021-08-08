9ef5701a2757a3e1ffb0f015d185a058594b28f0
Shalini Sharma <shalini07061997@gmsail.com>
2021-08-08 05:44:20 +0530

    live chat implmennted

--- Frontend/tutortown/src/App.js
+++ Frontend/tutortown/src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       {/* < HomeMainPage /> */}
-      {/* <Routes/> */}
-      < StudentMainPage />
+      <Routes/>
+      {/* < StudentMainPage /> */}
     </div>
   );
 }
--- Frontend/tutortown/src/Routes/Route.jsx
+++ Frontend/tutortown/src/Routes/Route.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Admin } from "../Components/Admin/Admin";
+import HomeMainPage from "../Components/HomePage/HomeMainPage";
 
 const Routes = () => {
   return (
     <div>
       <Switch>
+        <Route exact path="/">
+          <HomeMainPage />
+        </Route>
         <Route path="/admin/tutorTown/authenticated" exact>
           <Admin />
         </Route>
