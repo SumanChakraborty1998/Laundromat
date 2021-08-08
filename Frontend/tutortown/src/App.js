15204fbd469bce1824448f5f73581e2def1d6d43
Ashwani-Sahu <sahua5979@gmail.com>
2021-08-08 08:40:22 +0530

    Updated UI

--- Frontend/tutortown/src/App.js
+++ Frontend/tutortown/src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
-import HomeMainPage from "./Components/HomePage/HomeMainPage";
 import Routes from "./Routes/Route";
-import StudentMainPage from "./Components/StudentPage/StudentMainPage";
 
 function App() {
     return (
