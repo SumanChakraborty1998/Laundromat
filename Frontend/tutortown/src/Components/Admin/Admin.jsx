11de396337c6db940aa084826c8ac6a4c7f01590
Shalini Sharma <shalini07061997@gmsail.com>
2021-08-08 07:13:31 +0530

    pull

--- Frontend/tutortown/src/Components/Admin/Admin.jsx
+++ Frontend/tutortown/src/Components/Admin/Admin.jsx
@@ -3,11 +3,11 @@ import { Main } from "./VideoVerification/Main";
 import { ContextProvider } from "../../Context/SocketContext";
 
 export const Admin = () => {
-    const [verify, setVerify] = React.useState(false);
-    return (
-        // <ContextProvider>
-        //     <Main />
-        // </ContextProvider>
-        <></>
-    );
+  const [verify, setVerify] = React.useState(false);
+  return (
+    <ContextProvider>
+      <Main />
+    </ContextProvider>
+    //<></>
+  );
 };
